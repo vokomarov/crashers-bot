2cd9b03874c981b73fd1cfd260133cab2602a824
Volodymyr Komarov <vovikems@gmail.com>
2022-07-25 23:03:29 +0300

    Add stats commands to bot

---
+++ app/Telegram/Commands/PidarWeekCommand.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Telegram\Commands;
+
+use App\Models\User;
+use Carbon\Carbon;
+use Illuminate\Database\Eloquent\Collection;
+
+class PidarWeekCommand extends PidarAllCommand
+{
+    /**
+     * @var string
+     */
+    protected $name = 'pidarweek';
+
+    /**
+     * @var string
+     */
+    protected $description = 'Statistics of a pidar game by current week';
+
+    /**
+     * @var string
+     */
+    protected $usage = '/pidarweek';
+
+    /**
+     * @var string
+     */
+    protected $version = '1.0.0';
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Collection<User>
+     */
+    protected function getLuckiest(): Collection
+    {
+        return $this->getLuckiestQuery()
+                    ->where(
+                        'created_at',
+                        '>=',
+                        Carbon::today()->startOf('week', Carbon::MONDAY)
+                    )
+                    ->get();
+    }
+
+    protected function getMessageHeader(): string
+    {
+        return $this->lang('telegram.pidar-week-header');
+    }
+}
